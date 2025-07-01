74573f48758ef291083881dc51b4606c3e947142
86130 <1027209586@qq.com>
2025-07-01 11:28:10 +0800

    收藏

---
+++ src/main/java/cn/com/chinahitech/bjmarket/course/Service/FavoriteService.java
@@ -0,0 +1,7 @@
+package cn.com.chinahitech.bjmarket.course.Service;
+
+import cn.com.chinahitech.bjmarket.course.entity.Favorite;
+
+public interface FavoriteService {
+    int addFavorite(Favorite favorite);
+}
