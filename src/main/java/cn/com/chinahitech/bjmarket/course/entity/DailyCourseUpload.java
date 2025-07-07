86b7271f0f39324017ef1f11a191beb8256ac473
86130 <1027209586@qq.com>
2025-07-07 10:03:26 +0800

    优化

--- src/main/java/cn/com/chinahitech/bjmarket/course/entity/DailyCourseUpload.java
+++ src/main/java/cn/com/chinahitech/bjmarket/course/entity/DailyCourseUpload.java
@@ -1,9 +1,12 @@
 package cn.com.chinahitech.bjmarket.course.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class DailyCourseUpload {
+    @TableId(value = "date", type = IdType.AUTO)
     private String date;
     private Integer courseCount;
 }
--- src/main/java/cn/com/chinahitech/bjmarket/login/entity/DailyVisits.java
+++ src/main/java/cn/com/chinahitech/bjmarket/login/entity/DailyVisits.java
@@ -1,5 +1,7 @@
 package cn.com.chinahitech.bjmarket.login.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
@@ -7,6 +9,7 @@ import java.sql.Timestamp;
 
 @Data
 public class DailyVisits {
+    @TableId(value = "date", type = IdType.AUTO)
     private String date;
     private Integer count;
 }
